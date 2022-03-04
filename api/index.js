16aa2070acea81cdd2962eecfb4524c3d5aa51c2
danaemeneses <danaemeneses5@gmail.com>
2022-03-04 16:29:47 -0300

    empty commit

--- api/index.js
+++ api/index.js
@@ -22,8 +22,10 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 /////////////cambbio
+const PORT = process.env.PORT || 3001;
+
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log("%s listening at 3000"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
